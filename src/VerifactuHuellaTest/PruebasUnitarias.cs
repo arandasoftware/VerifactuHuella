1b9430c1abe0dd8e4b4f7ef18a45496d42a01a18
arandafra <arandafra@outlook.es>
2024-11-16 15:21:56 +0100

    Incluir paquete TrxLogger

--- src/VerifactuHuellaTest/PruebasUnitarias.cs
+++ src/VerifactuHuellaTest/PruebasUnitarias.cs
@@ -1,125 +1,124 @@
 using VerifactuHuella;
 using VerifactuHuella.Models;
 
-namespace VerifactuHuellaTest
+namespace VerifactuHuellaTest;
+
+public class PruebasUnitarias
 {
-    public class PruebasUnitarias
+    [Fact]
+    public void CalcularHuella_RegistroAlta_ArgumentNullException()
     {
-        [Fact]
-        public void CalcularHuella_RegistroAlta_ArgumentNullException()
+        Huella _huella = new();
+        Assert.Throws<ArgumentNullException>(() => _huella.CalcularHuella<RegistroAlta>(null));
+    }
+
+    /// <summary>
+    /// Caso 1: primer registro de facturaciÃ³n â€“en este caso, de altaâ€“ en un Sistema InformÃ¡tico de FacturaciÃ³n(SIF)
+    /// </summary>
+    [Fact]
+    public void CalcularHuella_Primer_RegistroAlta()
+    {
+        Huella _huella = new();
+        string resultado = _huella.CalcularHuella(new RegistroAlta()
         {
-            Huella _huella = new();
-            Assert.Throws<ArgumentNullException>(() => _huella.CalcularHuella<RegistroAlta>(null));
-        }
+            IDEmisorFactura = "89890001K",
+            NumSerieFactura = "12345678/G33",
+            FechaExpedicionFactura = new DateTime(2024, 1, 1, 5, 0, 30, DateTimeKind.Local),
+            TipoFactura = "F1",
+            CuotaTotal = 12.35m,
+            ImporteTotal = 123.45m,
+            Huella = "",
+            FechaHoraHusoGenRegistro = new DateTime(2024, 1, 1, 19, 20, 30, DateTimeKind.Local),
+        });
 
-        /// <summary>
-        /// Caso 1: primer registro de facturación –en este caso, de alta– en un Sistema Informático de Facturación(SIF)
-        /// </summary>
-        [Fact]
-        public void CalcularHuella_Primer_RegistroAlta()
+        Assert.Equal("3C464DAF61ACB827C65FDA19F352A4E3BDC2C640E9E9FC4CC058073F38F12F60", resultado);
+    }
+
+    /// <summary>
+    /// Caso 2: registro de facturaciÃ³n -en este caso de alta- con registro de facturaciÃ³n anterior existente en el SIF (siendo, por tanto, el segundo registro o sucesivo)
+    /// </summary>
+    [Fact]
+    public void CalcularHuella_Sucesivos_RegistrosAlta()
+    {
+        Huella _huella = new();
+        string resultado = _huella.CalcularHuella(new RegistroAlta()
         {
-            Huella _huella = new();
-            string resultado = _huella.CalcularHuella(new RegistroAlta()
-            {
-                IDEmisorFactura = "89890001K",
-                NumSerieFactura = "12345678/G33",
-                FechaExpedicionFactura = new DateTime(2024, 1, 1, 5, 0, 30, DateTimeKind.Local),
-                TipoFactura = "F1",
-                CuotaTotal = 12.35m,
-                ImporteTotal = 123.45m,
-                Huella = "",
-                FechaHoraHusoGenRegistro = new DateTime(2024, 1, 1, 19, 20, 30, DateTimeKind.Local),
-            });
+            IDEmisorFactura = "89890001K",
+            NumSerieFactura = "12345679/G34",
+            FechaExpedicionFactura = new DateTime(2024, 1, 1, 5, 0, 30, DateTimeKind.Local),
+            TipoFactura = "F1",
+            CuotaTotal = 12.35m,
+            ImporteTotal = 123.45m,
+            Huella = "3C464DAF61ACB827C65FDA19F352A4E3BDC2C640E9E9FC4CC058073F38F12F60",
+            FechaHoraHusoGenRegistro = new DateTime(2024, 1, 1, 19, 20, 35, DateTimeKind.Local),
+        });
 
-            Assert.Equal("3C464DAF61ACB827C65FDA19F352A4E3BDC2C640E9E9FC4CC058073F38F12F60", resultado);
-        }
+        Assert.Equal("F7B94CFD8924EDFF273501B01EE5153E4CE8F259766F88CF6ACB8935802A2B97", resultado);
+    }
 
-        /// <summary>
-        /// Caso 2: registro de facturación -en este caso de alta- con registro de facturación anterior existente en el SIF (siendo, por tanto, el segundo registro o sucesivo)
-        /// </summary>
-        [Fact]
-        public void CalcularHuella_Sucesivos_RegistrosAlta()
+
+    /// <summary>
+    /// Caso 3: registro de facturaciÃ³n â€“en este caso, de anulaciÃ³nâ€“ con registro de facturaciÃ³n anterior existente en el SIF(siendo, por tanto, el segundo registro o sucesivo)
+    /// </summary>
+    [Fact]
+    public void CalcularHuella_Sucesivos_RegistrosAnulacion()
+    {
+        Huella _huella = new();
+        string resultado = _huella.CalcularHuella(new RegistroAnulacion()
         {
-            Huella _huella = new();
-            string resultado = _huella.CalcularHuella(new RegistroAlta()
-            {
-                IDEmisorFactura = "89890001K",
-                NumSerieFactura = "12345679/G34",
-                FechaExpedicionFactura = new DateTime(2024, 1, 1, 5, 0, 30, DateTimeKind.Local),
-                TipoFactura = "F1",
-                CuotaTotal = 12.35m,
-                ImporteTotal = 123.45m,
-                Huella = "3C464DAF61ACB827C65FDA19F352A4E3BDC2C640E9E9FC4CC058073F38F12F60",
-                FechaHoraHusoGenRegistro = new DateTime(2024, 1, 1, 19, 20, 35, DateTimeKind.Local),
-            });
+            IDEmisorFacturaAnulada = "89890001K",
+            NumSerieFacturaAnulada = "12345679/G34",
+            FechaExpedicionFacturaAnulada = new DateTime(2024, 1, 1, 5, 0, 30, DateTimeKind.Local),
+            Huella = "F7B94CFD8924EDFF273501B01EE5153E4CE8F259766F88CF6ACB8935802A2B97",
+            FechaHoraHusoGenRegistro = new DateTime(2024, 1, 1, 19, 20, 40, DateTimeKind.Local),
+        });
 
-            Assert.Equal("F7B94CFD8924EDFF273501B01EE5153E4CE8F259766F88CF6ACB8935802A2B97", resultado);
-        }
+        Assert.Equal("177547C0D57AC74748561D054A9CEC14B4C4EA23D1BEFD6F2E69E3A388F90C68", resultado);
+    }
 
-
-        /// <summary>
-        /// Caso 3: registro de facturación –en este caso, de anulación– con registro de facturación anterior existente en el SIF(siendo, por tanto, el segundo registro o sucesivo)
-        /// </summary>
-        [Fact]
-        public void CalcularHuella_Sucesivos_RegistrosAnulacion()
+    /// <summary>
+    /// Caso 4: primer registro de evento
+    /// </summary>
+    [Fact]
+    public void CalcularHuella_Primer_RegistrosEvento()
+    {
+        Huella _huella = new();
+        string resultado = _huella.CalcularHuella(new RegistroEvento()
         {
-            Huella _huella = new();
-            string resultado = _huella.CalcularHuella(new RegistroAnulacion()
-            {
-                IDEmisorFacturaAnulada = "89890001K",
-                NumSerieFacturaAnulada = "12345679/G34",
-                FechaExpedicionFacturaAnulada = new DateTime(2024, 1, 1, 5, 0, 30, DateTimeKind.Local),
-                Huella = "F7B94CFD8924EDFF273501B01EE5153E4CE8F259766F88CF6ACB8935802A2B97",
-                FechaHoraHusoGenRegistro = new DateTime(2024, 1, 1, 19, 20, 40, DateTimeKind.Local),
-            });
+            NIF = "89890001K",
+            ID = "",
+            IdSistemaInformatico = "123456",
+            Version = "1.1",
+            NumeroInstalacion = "1",
+            NIFEmisior = "89890001K",
+            TipoEvento = "A",
+            HuellaEvento = "",
+            FechaHoraHusoGenEvento = new DateTime(2024, 1, 1, 19, 20, 40, DateTimeKind.Local),
+        });
 
-            Assert.Equal("177547C0D57AC74748561D054A9CEC14B4C4EA23D1BEFD6F2E69E3A388F90C68", resultado);
-        }
+        Assert.Equal("A632D1BB507D1013F76FC81AF984C3510B9D33F00ECE4826C96F5BB22524780E", resultado);
+    }
 
-        /// <summary>
-        /// Caso 4: primer registro de evento
-        /// </summary>
-        [Fact]
-        public void CalcularHuella_Primer_RegistrosEvento()
+    /// <summary>
+    /// Caso 5: registro de evento con registro de evento anterior existente en el SIF(siendo, por tanto, el segundo registro o sucesivo)
+    /// </summary>
+    [Fact]
+    public void CalcularHuella_Sucesivo_RegistrosEvento()
+    {
+        Huella _huella = new();
+        string resultado = _huella.CalcularHuella(new RegistroEvento()
         {
-            Huella _huella = new();
-            string resultado = _huella.CalcularHuella(new RegistroEvento()
-            {
-                NIF = "89890001K",
-                ID = "",
-                IdSistemaInformatico = "123456",
-                Version = "1.1",
-                NumeroInstalacion = "1",
-                NIFEmisior = "89890001K",
-                TipoEvento = "A",
-                HuellaEvento = "",
-                FechaHoraHusoGenEvento = new DateTime(2024, 1, 1, 19, 20, 40, DateTimeKind.Local),
-            });
+            NIF = "89890001K",
+            ID = "",
+            IdSistemaInformatico = "123456",
+            Version = "1.1",
+            NumeroInstalacion = "1",
+            NIFEmisior = "89890001K",
+            TipoEvento = "A",
+            HuellaEvento = "A632D1BB507D1013F76FC81AF984C3510B9D33F00ECE4826C96F5BB22524780E",
+            FechaHoraHusoGenEvento = new DateTime(2024, 1, 1, 19, 20, 50, DateTimeKind.Local),
+        });
 
-            Assert.Equal("A632D1BB507D1013F76FC81AF984C3510B9D33F00ECE4826C96F5BB22524780E", resultado);
-        }
-
-        /// <summary>
-        /// Caso 5: registro de evento con registro de evento anterior existente en el SIF(siendo, por tanto, el segundo registro o sucesivo)
-        /// </summary>
-        [Fact]
-        public void CalcularHuella_Sucesivo_RegistrosEvento()
-        {
-            Huella _huella = new();
-            string resultado = _huella.CalcularHuella(new RegistroEvento()
-            {
-                NIF = "89890001K",
-                ID = "",
-                IdSistemaInformatico = "123456",
-                Version = "1.1",
-                NumeroInstalacion = "1",
-                NIFEmisior = "89890001K",
-                TipoEvento = "A",
-                HuellaEvento = "A632D1BB507D1013F76FC81AF984C3510B9D33F00ECE4826C96F5BB22524780E",
-                FechaHoraHusoGenEvento = new DateTime(2024, 1, 1, 19, 20, 50, DateTimeKind.Local),
-            });
-
-            Assert.Equal("0F91A8BD213777015492BFD57577CFE8F557701DA1F948A8B93924FF063D948A", resultado);
-        }
+        Assert.Equal("0F91A8BD213777015492BFD57577CFE8F557701DA1F948A8B93924FF063D948A", resultado);
     }
 }
